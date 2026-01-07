ba4fa7e54e7d6ab49c6df6756257ab752cf2978b
jackbui <dung.bui@speedydd.com>
2026-01-07 14:04:08 +0700

    Udpate bot config

--- apps/be/src/bot/bot.module.ts
+++ apps/be/src/bot/bot.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
-import { NezonModule } from '@nezon';
 import { DatabaseModule } from '@src/common/database/database.module';
 import { AppConfigService } from '@src/common/shared/services/app-config.service';
 import { SharedModule } from '@src/common/shared/shared.module';
+import { NezonModule } from '@src/libs/nezon';
 
 @Module({
   imports: [
