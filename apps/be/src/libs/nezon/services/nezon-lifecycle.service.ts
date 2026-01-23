de08ccd79b6181fae5f9a17c4fba2a475a4db75f
jackbui <dung.bui@speedydd.com>
2026-01-23 09:09:37 +0700

    Hotfix: Service run when bot die

--- apps/be/src/libs/nezon/services/nezon-lifecycle.service.ts
+++ apps/be/src/libs/nezon/services/nezon-lifecycle.service.ts
@@ -1,17 +1,20 @@
 import {
   Injectable,
+  Logger,
   OnApplicationBootstrap,
   OnApplicationShutdown,
 } from '@nestjs/common';
 import { NezonClientService } from '../client/nezon-client.service';
 import { NezonCommandService } from './nezon-command.service';
-import { NezonEventsService } from './nezon-events.service';
 import { NezonComponentService } from './nezon-component.service';
+import { NezonEventsService } from './nezon-events.service';
 
 @Injectable()
 export class NezonLifecycleService
   implements OnApplicationBootstrap, OnApplicationShutdown
 {
+  private readonly logger = new Logger(NezonLifecycleService.name);
+
   constructor(
     private readonly clientService: NezonClientService,
     private readonly commandService: NezonCommandService,
@@ -19,11 +22,24 @@ export class NezonLifecycleService
     private readonly componentService: NezonComponentService,
   ) {}
 
-  async onApplicationBootstrap() {
-    await this.clientService.login();
-    this.eventsService.initialize();
-    await this.commandService.initialize();
-    this.componentService.initialize();
+  onApplicationBootstrap() {
+    this.initializeBotBackground();
+  }
+
+  private async initializeBotBackground() {
+    this.logger.log('Initializing Bot in background...');
+    try {
+      await this.clientService.login();
+      this.eventsService.initialize();
+      await this.commandService.initialize();
+      this.componentService.initialize();
+      this.logger.log('Bot connected and initialized successfully.');
+    } catch (error) {
+      this.logger.error(
+        'Failed to initialize Nezon Bot. Proceeding without Bot functionalities.',
+        error,
+      );
+    }
   }
 
   async onApplicationShutdown() {
